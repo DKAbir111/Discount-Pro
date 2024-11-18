5a56eae0de30b6270e5afd794b0dd645296296ba
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-18 23:37:44 +0600

    Basic setup for Discount pro project

---
+++ src/Routes/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Hello from Home</h1>
+
+    }
+])
+
+export default router;
